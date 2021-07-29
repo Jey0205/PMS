d831ee24552b138e02a0bde0c35f3a9d8948ba16
Jey0205 <daedra02@gmail.com>
2021-07-30 03:28:12 +0700

    add user done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 const helpers = require('../helper/util')
+const bcrypt = require('bcrypt');
+const salt = 5;
+
 
 
 module.exports = function (db) {
@@ -33,25 +36,76 @@ module.exports = function (db) {
         if (page) {
             querys = `select projects.projectid, projects.name, ARRAY_AGG(' ' || users.firstname) as members FROM members INNER JOIN users USING (userid) INNER JOIN projects USING (projectid) group by projects.projectid, projects.name order by projects.projectid limit 3 offset ${offset}`
         }
+        if (params.length > 0) {
+            querys = `select projects.projectid, projects.name, ARRAY_AGG(' ' || users.firstname) as members FROM members INNER JOIN users USING (userid) INNER JOIN projects USING (projectid)`;
+            queryCount = `select projects.projectid, projects.name, ARRAY_AGG(' ' || users.firstname) as members FROM members INNER JOIN users USING (userid) INNER JOIN projects USING (projectid)`;
 
-        db.query(queryCount, [], (err, data) => {
-            let totalPage = data.rows.length;
-            let pages = Math.ceil(totalPage / limitTab)
-            if (err) {
-                throw err
+            querys += ` where ${params.join(" and ")} group by projects.projectid , projects.name order by projects.projectid limit 3 offset ${offset}`
+
+            queryCount += ` where ${params.join(" and ")} group by projects.projectid, projects.name order by projects.projectid`
+        }
+
+        db.query(queryCount, (err, result) => {
+            let total = result.rows.length;
+            let pages = Math.ceil(total / 3);
+            db.query(querys, (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                db.query(
+                    "select option from users where userid = $1",
+                    [req.session.user.userid],
+                    (err, option) => {
+                        if (err) {
+                            throw err;
+                        }
+                        db.query("select * from users", (err, names) => {
+                            if (err) {
+                                throw err;
+                            }
+                            res.render("../views/index", {
+                                data: data.rows,
+                                option: option.rows[0].options,
+                                names: names.rows,
+                                projectid,
+                                members,
+                                name,
+                                pages,
+                                page,
+                                url,
+                                session: req.session.user
+                            });
+                        });
+                    }
+                );
+            });
+        });
+    });
+
+
+
+    /* GET register page. */
+    router.get('/register', function (req, res, next) {
+        res.render('../views/login/register', { title: 'Express' });
+    });
+    router.post('/register', function (req, res, next) {
+        bcrypt.hash(req.body.password, salt, function (err, hash) {
+            if (req.body.typeFt == true) {
+                db.query(`insert into users(email,password,firstname,lastname,isfulltime, isparttime,position) values ('${req.body.email}','${hash}','${req.body.firstname}','${req.body.lastname}', true, false,'${req.body.position}')`, [])
+                    .then(res.redirect('/'))
+                    .catch(e => console.error(e));
+            } else {
+                db.query(`insert into users(email,password,firstname,lastname,isfulltime, isparttime,position) values ('${req.body.email}','${hash}','${req.body.firstname}','${req.body.lastname}', false, true ,'${req.body.position}')`, [])
+                    .then(res.redirect('/'))
+                    .catch(e => console.error(e));
+
             }
-            db.query(querys)
-                .then(data => {
-                    res.render('../views/index', { data: data.rows, totalPage, projectid, name, members, page, pages, url })
-                })
         })
-
-
     });
 
 
     /* Logout */
-    router.get('/logout', helpers.isLoggedIn, function (req, res, next) {
+    router.get('/logout', helpers.isLoggedIn, (req, res, next) => {
         req.session.destroy.then(res.redirect('/login'))
             .catch(err => console.error(err))
 
--- routes/profile.js
+++ routes/profile.js
@@ -7,44 +7,64 @@ const salt = 5;
 
 module.exports = function (db) {
     /* Profile */
-    router.get('/', helpers.isLoggedIn,(req, res, next) => {
-        res.render('../views/profile/profile', { email: req.session.user })
+    router.get('/', helpers.isLoggedIn, (req, res, next) => {
+        db.query(`select * from users where email = $1`, [req.session.user.email])
+        res.render('../views/profile/profile', { email: req.session.user.email, info: req.flash('info'), session: req.session.user })
     })
-    router.post('/', helpers.isLoggedIn,(req, res, next) => {
-        
-            db.query(`update users set position = $1, isfulltime = true isparttime = false where email = $2`)
+    router.post('/', helpers.isLoggedIn, (req, res, next) => {
+        console.log(req.body.options)
+        if (req.body.typeFt == undefined) {
+            return db.query(`update users set position = $1, isfulltime = false, isparttime = true where email = $2`, [req.body.options, req.session.user.email], (err, data) => {
+                if (err) throw err
+                req.flash('info', 'Update Successful')
+                res.redirect('/')
+
+            })
+
+        } else {
+            db.query(`update users set position = $1, isfulltime = true, isparttime = false where email = $2`, [req.body.options, req.session.user.email], (err, data) => {
+                if (err) throw err
+                req.flash('info', 'Update Successful')
+                res.redirect('/')
+
+            })
+        }
     })
-    router.get('/password', helpers.isLoggedIn,(req, res, next) => {
+
+
+    /* Password */
+    router.get('/password', helpers.isLoggedIn, (req, res, next) => {
         res.render('../views/profile/password', { info: req.flash('info') })
     })
-    router.post('/password', helpers.isLoggedIn, (req, res, next) => { console.log(req.body.pass1)
+    router.post('/password', helpers.isLoggedIn, (req, res, next) => {
+        console.log(req.body.pass1)
         db.query(`select * from users where email = $1`, [req.session.user.email], (err, data) => {
-            bcrypt.compare(req.body.pass1, data.rows[0].password, (err,result) =>{
-             if(err){
-                 req.flash('info','Something wrong, dude!')
-                 return res.redirect('/profile/password')
-             }
-             if(req.body.pass2 !== req.body.pass3){
-                 req.flash('info', 'The password is not equal!')
-                 return res.redirect('/profile/password')
-             }
-             if(result){
-                 if(req.body.pass2 == req.body.pass3){
-                     bcrypt.hash(req.body.pass2,salt, (err,hash) =>{
-                         db.query('update users set password = $1, email = $2',[hash, req.session.user.email], (err,data) =>{
-                             if(err){
-                                 throw err
-                             }
-                             if(data){
-                                 req.flash('info', 'Password has change')
-                                 return res.redirect('/')
-                             }
-                         })
-                       
-                     })
-                 }
-             }
-         })
+            bcrypt.compare(req.body.pass1, data.rows[0].password, (err, result) => {
+                if (err) {
+                    req.flash('info', 'Old password is wrong!')
+                    return res.redirect('/profile/password')
+                }
+                if (req.body.pass2 !== req.body.pass3) {
+                    req.flash('info', 'The password is not equal!')
+                    return res.redirect('/profile/password')
+                }
+                if (result) {
+                    if (req.body.pass2 == req.body.pass3) {
+                        bcrypt.hash(req.body.pass2, salt, (err, hash) => {
+                            db.query('update users set password = $1, email = $2', [hash, req.session.user.email], (err, data) => {
+                                if (err) {
+                                    throw err
+                                }
+                                if (data) {
+                                    req.flash('info', 'Password has change')
+                                    return res.redirect('/')
+                                }
+                            })
+
+                        })
+                    }
+                }
+            })
         })
     })
     return router
